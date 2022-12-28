83e62582c8314dbac8d11639e02213bf2291534f
shariqansari88 <36728316+ShariqAnsari88@users.noreply.github.com>
2022-12-29 04:56:46 +0530

    enpoint changed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
 
     const fetchBotResponse = async () => {
         const { data } = await axios.post(
-            "http://localhost:4000",
+            "https://chatgpt-ai-app-od21.onrender.com",
             { input },
             {
                 headers: {
