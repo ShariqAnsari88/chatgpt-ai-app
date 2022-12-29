c24773b343dbe08d7f3cacb4fa4fdb114dc8589b
shariqansari88 <36728316+ShariqAnsari88@users.noreply.github.com>
2022-12-29 20:51:27 +0530

    final commit for server code

--- api/server.js
+++ api/server.js
@@ -1,15 +1,9 @@
 import express from "express";
-import * as dotenv from "dotenv";
 import cors from "cors";
+import * as dotenv from "dotenv";
 import { Configuration, OpenAIApi } from "openai";
 
 dotenv.config();
-
-const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-});
-
-const openai = new OpenAIApi(configuration);
 
 const app = express();
 app.use(cors());
@@ -17,32 +11,36 @@ app.use(express.json());
 
 app.get("/", async (req, res) => {
     res.status(200).send({
-        message: "Hello from JSDEV",
+        message:
+            "This is ChatGPT AI APP server url, please visit http://localhost:5173",
     });
 });
 
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
 app.post("/", async (req, res) => {
     try {
-        const input = req.body.input;
-
         const response = await openai.createCompletion({
             model: "text-davinci-003",
-            prompt: `${input}`,
-            temperature: 0, // Higher values means the model will take more risks.
-            max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
-            top_p: 1, // alternative to sampling with temperature, called nucleus sampling
-            frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
-            presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+            prompt: req.body.input,
+            temperature: 0,
+            max_tokens: 4000,
+            top_p: 1,
+            frequency_penalty: 0.5,
+            presence_penalty: 0,
         });
-        console.log("server pass", input);
+        console.log("PASSED: ", req.body.input);
 
         res.status(200).send({
             bot: response.data.choices[0].text,
         });
     } catch (error) {
-        console.log("server fail", req.body.input);
+        console.log("FAILED:", req.body.input);
         console.error(error);
-        res.status(500).send(error || "Something went wrong");
+        res.status(500).send(error);
     }
 });
 
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,6 +18,7 @@ function App() {
     const fetchBotResponse = async () => {
         const { data } = await axios.post(
             "https://chatgpt-ai-app-od21.onrender.com",
+            // "http://localhost:4000",
             { input },
             {
                 headers: {
